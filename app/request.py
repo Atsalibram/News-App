90c9f45a21b5301722acd02efa6b59014b80180e
atsalibram <atsalibaram@gmail.com>
2021-02-24 13:34:04 +0300

    access app config objects

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+from app import app
+import urllib.request,json
+from .models import movie
 
+News = news.News
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["News_API_BASE_URL"]
