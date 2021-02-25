6b43337714947b1178c189b187ab72c3e941d26a
atsalibram <atsalibaram@gmail.com>
2021-02-25 14:44:43 +0300

    update nanage.py

--- config.py
+++ config.py
@@ -10,24 +10,12 @@ class Config:
     SECRET_KEY = os.environ.get('Bram')
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
