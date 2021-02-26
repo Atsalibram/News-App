780eac322cfda87306a3ea77f20796aaa86c62ce
atsalibram <atsalibaram@gmail.com>
2021-02-26 08:11:00 +0300

    changed title

--- app/main/views.py
+++ app/main/views.py
@@ -14,7 +14,7 @@ def index():
     all_sources = get_news_source()
     all_news = get_everything()
     tech_stories = tech_headlines()
-    title = 'Falling Sky News'
+    title = 'World Wide News'
 
     return render_template('index.html', sources = all_sources, others = all_news, tech = tech_stories, title = title)
 
@@ -24,6 +24,6 @@ def news_headlines(source):
     Function pulls/gets the top and breakng news
     '''
 
-    title = "Falling Sky News"
+    title = "World Wide News"
     news_headlines = get_news_headlines(source)
     return render_template('articles.html', title = title, headlines = news_headlines)
